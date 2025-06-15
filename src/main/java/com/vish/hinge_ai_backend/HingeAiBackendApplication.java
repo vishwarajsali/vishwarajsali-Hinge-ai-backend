f8b4e9ac79cd1cdac392977cce8a66b09fa3d847
vish <sali.vishwaraj@gmail.com>
2025-06-14 21:34:59 -0400

    Basic Initializer app in working state

---
+++ src/main/java/com/vish/hinge_ai_backend/HingeAiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vish.hinge_ai_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HingeAiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HingeAiBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vish/hinge_ai_backend/HingeAiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vish.hinge_ai_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HingeAiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
