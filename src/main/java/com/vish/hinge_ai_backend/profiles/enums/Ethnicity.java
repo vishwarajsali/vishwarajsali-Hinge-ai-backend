5cbd2ab402b4572e17e68adff5fb735701a394de
vish <sali.vishwaraj@gmail.com>
2025-06-14 22:52:57 -0400

    Updated the Enum description Ethnicity

--- src/main/java/com/vish/hinge_ai_backend/profiles/enums/Ethnicity.java
+++ src/main/java/com/vish/hinge_ai_backend/profiles/enums/Ethnicity.java
@@ -1,14 +1,24 @@
 package com.vish.hinge_ai_backend.profiles.enums;
 
 public enum Ethnicity {
-    ASIAN,
-    BLACK_OR_AFRICAN_AMERICAN,
-    HISPANIC_OR_LATINO,
-    WHITE,
-    NATIVE_AMERICAN_OR_ALASKA_NATIVE,
-    NATIVE_HAWAIIAN_OR_OTHER_PACIFIC_ISLANDER,
-    MIDDLE_EASTERN_OR_NORTH_AFRICAN,
-    TWO_OR_MORE_RACES,
-    OTHER,
-    PREFER_NOT_TO_SAY
+    ASIAN("Asian"),
+    BLACK_OR_AFRICAN_AMERICAN("Black or African American"),
+    HISPANIC_OR_LATINO("Hispanic or Latino"),
+    WHITE("White"),
+    NATIVE_AMERICAN_OR_ALASKA_NATIVE("Native American or Alaska Native"),
+    NATIVE_HAWAIIAN_OR_OTHER_PACIFIC_ISLANDER("Native Hawaiian or Other Pacific Islander"),
+    MIDDLE_EASTERN_OR_NORTH_AFRICAN("Middle Eastern or North African"),
+    TWO_OR_MORE_RACES("Two or More Races"),
+    OTHER("Other"),
+    PREFER_NOT_TO_SAY("Prefer not to say");
+
+    private final String description;
+
+    Ethnicity(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
